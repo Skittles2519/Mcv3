105f1b362237361cf86bfcb9f89082f12cdd1a9a
Hunter Kaleb Perry <skittles2519@hotmail.com>
2020-03-07 17:25:54 -0700

    model

---
+++ Mcv3/Data/Mcv3Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mcv3.Models
+{
+    public class Mcv3Context : DbContext
+    {
+        public Mcv3Context (DbContextOptions<Mcv3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mcv3.Models.Movie> Movie { get; set; }
+    }
+}
--- Mcv3/Startup.cs
+++ Mcv3/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Mcv3.Models;
 
 namespace Mcv3
 {
@@ -33,6 +35,9 @@ namespace Mcv3
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<Mcv3Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Mcv3Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
